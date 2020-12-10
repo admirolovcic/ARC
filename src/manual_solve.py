a3011443f3b5022adb80767c33bd59db1f98a206
Olovcic <admirolovcic@joneseng.com>
2020-12-10 22:28:26 +0000

    commiting task 2

--- src/manual_solve.py
+++ src/manual_solve.py
@@ -4,20 +4,73 @@ import os, sys
 import json
 import numpy as np
 import re
+from collections import Counter
 
 ### YOUR CODE HERE: write at least three functions which solve
 ### specific tasks by transforming the input x and returning the
 ### result. Name them according to the task ID as in the three
 ### examples below. Delete the three examples. The tasks you choose
 ### must be in the data/training directory, not data/evaluation.
-def solve_6a1e5592(x):
+"""
+def solve_3631a71a(x):
+    width = 29
+    height = 29
+    for i in range(width +1):
+        for j in range(height+1):
+            if(x[i,j] == 9):
+                if(i<2 & j<2):
+                    pass
+                elif(i<2):
+                    x[i,j] = x[i, width - j - 2]
+                elif(j<2):
+                    x[i,j] = x[height - i + 2, j]
+                else:  
+                    c1 = x[i, width - j + 2]
+                    c2 = x[height - i + 2, j]
+                    c3 = x[height - i + 2, width - j + 2]
+                    c = Counter([c1, c2, c3])
+                    most_common = c.most_common(1)[0][0]
+                    #print(f"{x[i,j]} changed to {most_common} at {i}, {j}" )
+                    x[i,j] = most_common
+                    
     return x
+"""
 
-def solve_b2862040(x):
-    return x
+def solve_681b3aeb(x):
+    flat_list = x.flatten()
+    c = Counter(flat_list)
+    most_common = c.most_common(3)
+    color1 = most_common[1][0]
+    color1_count = most_common[1][1]
+    color2 = most_common[2][0]
+    color2_count = most_common[2][1]
+    
+    #resize array so it can more easily be traversed with a 3x3 array
+    zero_padding_v = np.zeros((2,14))
+    zero_padding_h = np.zeros((10,2))
+    resized_x = np.hstack((zero_padding_h, x, zero_padding_h))
+    resized_x = np.vstack((zero_padding_v, resized_x, zero_padding_v))
+    possible_color1_matrices = []
+    possible_color2_matrices = []
+    
+    for i in range(11):
+        for j in range(11):
+            arr_slice = resized_x[i:i+3,j:j+3]
+            count_of_color1 = list(arr_slice.flatten()).count(color1)
+            count_of_color2 = list(arr_slice.flatten()).count(color2)
+            if(count_of_color1 == color1_count):
+                possible_color1_matrices.append(arr_slice)
+            if(count_of_color2 == color2_count):
+                possible_color2_matrices.append(arr_slice)
+    
+    for m1 in possible_color1_matrices:
+        for m2 in possible_color2_matrices:
+            candidate = m1 + m2
+            if(list(candidate.flatten()).count(0) == 0):
+                return candidate
 
-def solve_05269061(x):
-    return x
+# def solve_05269061(x):
+#     return x
 
 
 def main():
